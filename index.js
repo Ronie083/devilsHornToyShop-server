9e1da06eded957bee7444c0d3f4caf06fc2ff509
Ronie083 <ahmedronie6@gmail.com>
2023-05-21 16:01:35 +0600

    single data loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -29,9 +29,19 @@ async function run() {
 
         const allToysCat = client.db('devilsHornToys').collection('alltoys');
 
-        app.get('/alltoys', async(req, res) =>{
+        app.get('/alltoys', async (req, res) => {
             const cursor = allToysCat.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/alltoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { "toys.toy_id": id };
+            const options = {
+                projection: { _id: 0, toys: { $elemMatch: { toy_id: id } } },
+            };
+            const result = await allToysCat.findOne(query, options);
             res.send(result);
         })
 
